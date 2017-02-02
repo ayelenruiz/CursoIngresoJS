404e4679bf1a4d0de3bf8537d73d90a5e8ef3a4c
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-02 20:26:53 -0300

    primer commit

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+    //nombre="araceli"
+    nombre=prompt("ingresa tu nombre");
+	alert(nombre);
+
+
+
+
+
+	//var person = prompt("ingresar tu nombre", "Natalia Natalia");
 }
 
