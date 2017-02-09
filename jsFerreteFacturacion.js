4474c7ba74d1c9a47e80e55b8f1bab747d6793aa
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-09 18:35:03 -0300

    f

--- jsFerreteFacturacion.js
+++ jsFerreteFacturacion.js
@@ -12,14 +12,40 @@ function Sumar ()
 	preciouno=document.getElementById('PrecioUno').value;
 	preciodos=document.getElementById('PrecioDos').value;
 	preciotres=document.getElementById('PrecioTres').value;
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);
 	resultado=preciouno+preciodos+preciotres;
 	alert("el resultado es" + resultado);
 }
 function Promedio () 
 {
-	
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var resultado;
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);
+	resultado=((preciouno+preciodos+preciotres)/3);
+	alert("el promedio es"+resultado);
+
 }
 function PrecioFinal () 
 {
-	
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var resultado;
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);
+	resultado=(preciouno+preciodos+preciotres)*1.10;
+	alert("el precio final es"+resultado);
 }
