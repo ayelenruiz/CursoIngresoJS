268ff82df2ecded7c0abfba1829b8adc2486c60f
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-09 13:47:54 -0300

    Add files via upload

---
+++ jsFerreteFacturacion.js
@@ -0,0 +1,25 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var resultado;
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+	resultado=preciouno+preciodos+preciotres;
+	alert("el resultado es" + resultado);
+}
+function Promedio () 
+{
+	
+}
+function PrecioFinal () 
+{
+	
+}
