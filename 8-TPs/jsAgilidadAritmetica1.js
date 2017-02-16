55342aa1eadcab9f13c45241710a4a3a96b4f4b3
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-16 16:51:59 -0300

    agi aritmetica 1 y 2

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,52 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var numerosecretouno;
+var numerosecretodos;
+var operacionrandom;
+var resultado;
+
 function comenzar()
 {
+	numerosecretouno=Math.floor(Math.random() * 10) + 1;
+	numerosecretodos=Math.floor(Math.random() * 10) + 1;
+	operacionrandom=Math.floor(Math.random() * 4) + 1;
+	document.getElementById('PrimerNumero').value=numerosecretouno;
+	document.getElementById('SegundoNumero').value=numerosecretodos;
+
+	if (operacionrandom==1)
+	{
+		document.getElementById('Operador').value="Suma";
+		resultado=numerosecretouno+numerosecretodos;
+	}
+	else if (operacionrandom==2)
+	{
+		document.getElementById('Operador').value="Resta";
+		resultado=numerosecretouno-numerosecretodos;
+	}
+	else if (operacionrandom==3)
+	{
+		document.getElementById('Operador').value="Division";
+		resultado=numerosecretouno/numerosecretodos;
+	}
+	else
+	{
+		document.getElementById('Operador').value="Multiplicacion";
+		resultado=numerosecretouno*numerosecretodos;
+	}
 
 	
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
-
+	respuesta=document.getElementById('Respuesta').value
+	if(respuesta==resultado)
+	{
+		alert("es correcto");
+	}
+	else if(respuesta!=resultado)
+	{
+		alert("es incorrecto");
+	}
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsAgilidadAritmetica2.js
+++ 8-TPs/jsAgilidadAritmetica2.js
@@ -5,13 +5,52 @@ se agregara un temporizador que
  correcto en ese lapso de tiempo. */
 var respuesta;
 var temporizador;
+var numerosecretouno;
+var numerosecretodos;
+var operacionrandom;
+var resultado;
+
 function comenzar()
 {
-	
+	numerosecretouno=Math.floor(Math.random() * 10) + 1;
+	numerosecretodos=Math.floor(Math.random() * 10) + 1;
+	operacionrandom=Math.floor(Math.random() * 4) + 1;
+	document.getElementById('PrimerNumero').value=numerosecretouno;
+	document.getElementById('SegundoNumero').value=numerosecretodos;
+
+	if (operacionrandom==1)
+	{
+		document.getElementById('Operador').value="Suma";
+		resultado=numerosecretouno+numerosecretodos;
+	}
+	else if (operacionrandom==2)
+	{
+		document.getElementById('Operador').value="Resta";
+		resultado=numerosecretouno-numerosecretodos;
+	}
+	else if (operacionrandom==3)
+	{
+		document.getElementById('Operador').value="Division";
+		resultado=numerosecretouno/numerosecretodos;
+	}
+	else
+	{
+		document.getElementById('Operador').value="Multiplicacion";
+		resultado=numerosecretouno*numerosecretodos;
+	}
+
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+	respuesta=document.getElementById('Respuesta').value
+	if(respuesta==resultado)
+	{
+		alert("es correcto");
+	}
+	else if(respuesta!=resultado)
+	{
+		alert("es incorrecto");
+	}
 
 
 }//FIN DE LA FUNCIÓN
