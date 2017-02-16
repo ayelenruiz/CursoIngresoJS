167998a29e32b3867469f8522303e245d0b9db5d
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-16 16:35:41 -0300

    Add files via upload

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,76 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var numerosecreto;
 
 function comenzar()
 {
 	
+	numerosecreto=Math.floor(Math.random() * 3) + 1;
+	switch(numerosecreto)
+	{
+		case 1:
+		eleccionMaquina="piedra";
+		break
+		case 2:
+		eleccionMaquina="papel";
+		break
+		case 3:
+		eleccionMaquina="tijera";
+	}
+	alert(eleccionMaquina);
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	eleccion="piedra";
+	if(eleccion==eleccionMaquina)
+	{
+		alert("empate");
+	}
+	if(eleccionMaquina=="tijera")
+	{
+		alert("ganaste");
+	}
+	if(eleccionMaquina=="papel")
+	{
+		alert("perdiste");
+	}
+
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+  eleccion="papel";
+	if(eleccion==eleccionMaquina)
+	{
+		alert("empate");
+	}
+	if(eleccionMaquina=="tijera")
+	{
+		alert("perdiste");
+	}
+	if(eleccionMaquina=="piedra")
+	{
+		alert("ganaste");
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	eleccion="tijera";
+	if(eleccion==eleccionMaquina)
+	{
+		alert("empate");
+	}
+	if(eleccionMaquina=="papel")
+	{
+		alert("ganaste");
+	}
+	if(eleccionMaquina=="piedra")
+	{
+		alert("perdiste");
+	}
 
 }//FIN DE LA FUNCIÓN
