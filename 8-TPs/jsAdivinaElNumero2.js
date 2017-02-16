4764f4ae5a45a6fa4d0a266ee8741d50b1d639ae
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-16 16:37:39 -0300

    numero2

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,79 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
-
+var numeroingresado;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
-	
+	numeroSecreto=Math.floor(Math.random() * 100) + 1;
+/*console.log("numerosecreto");*/
+		//alert(numeroSecreto );
+document.getElementById('intentos').value=contadorIntentos;
+
+		/*alert(numerosecreto);*/
+		console.log(numeroSecreto);
+		contadorIntentos=0;
 
 }
 
 function verificar()
 {
 	
-	
+	numeroingresado=document.getElementById('numero').value;
+    contadorIntentos=contadorIntentos+1;
+    document.getElementById('intentos').value=contadorIntentos;
+   
+			
+			
+			if (numeroingresado==numeroSecreto) 
+				{
+					alert("Sos un ganador");
+					switch(contadorIntentos)
+					{
+						case 1:
+						alert("Usted es un Psíquico");
+						break
+
+						case 2:
+						alert("excelente percepción");
+						break
+
+						case 3:
+						alert("Esto es suerte");
+						break
+
+						case 4:
+						alert("Excelente tecnica");
+						break
+
+						case 5:
+						alert("Usted esta en la media");
+						break
+
+						case 6:
+						case 7:
+						case 8:
+						case 9:
+						case 10:
+						alert("falta tecnica");
+						break
+
+						default:
+						alert("afortunado en el amor");
+
+
+
+					}
+				}
+			if (numeroingresado<numeroSecreto)
+				{
+					alert("te falta");
+				}	
+			if (numeroingresado>numeroSecreto)
+				{
+				    alert("te pasaste");
+				}
+
 
 }
