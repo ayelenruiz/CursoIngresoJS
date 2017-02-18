5a0da935f417ab6c8252cca2115afaaebf7da240
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-18 12:57:43 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,17 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
+	
+	while(respuesta == "si");
+	{
+        
+        numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		contador++
+		respuesta=prompt("ingrese si para continuar");
+		acumulador=acumulador+numero;
+	}
+	promedio=acumulador/contador;
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
