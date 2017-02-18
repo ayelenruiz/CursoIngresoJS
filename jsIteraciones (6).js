2925c37996f1521b68a20f068bb5c2310af99c36
ayelenruiz <ayelen_ruiz@hotmail.com>
2017-02-18 11:51:09 -0300

    Add files via upload

---
+++ jsIteraciones (6).js
@@ -0,0 +1,22 @@
+function Mostrar()
+{
+
+	var contador=0;
+	var acumulador=0;
+	var numero;
+	while(contador<5)
+	{
+		
+		numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+        contador++;
+	}
+	promedio=acumulador/5;
+
+
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/5;
+
+}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (7).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+
+	var contador=0;
+	var acumulador=0;
+	var respuesta='si';
+	var numero;
+	while(respuesta == "si");
+	{
+        contador++
+        numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		respuesta=prompt("ingrese si para continuar");
+		acumulador=acumulador+numero;
+	}
+	promedio=acumulador/contador;
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/contador;
+
+}//FIN DE LA FUNCIÓN
